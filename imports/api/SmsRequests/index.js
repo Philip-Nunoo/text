37960c0527ed6daaf1fb1fea4239c2f4d958e142
Philip Nunoo <phanunoo@gmail.com>
2018-12-20 10:38:15 +0000

    Send sms message

--- imports/api/SmsRequests/index.js
+++ imports/api/SmsRequests/index.js
@@ -1,4 +1,5 @@
 import { Mongo } from 'meteor/mongo';
+import { HTTP } from 'meteor/http';
 
 const SmsRequests = new Mongo.Collection('sms-requests');
 
@@ -14,6 +15,42 @@ SmsRequests.before.update(function (userId, doc, fieldNames, modifier, options) 
 
 SmsRequests.after.insert(function (userId, doc) {
     console.log('send sms message to', doc.to);
+    
+    if (doc.to) {
+        var request = require("request");
+
+        var options = {
+            method: 'POST',
+            url: 'https://www.txtconnect.co/v2/app/api/send/sms.json',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            form: {
+                token: 'ffd452df80dd8b78154bc886b9ee03593202c1fc',
+                message: doc.message,
+                sender: 'TSF - Test',
+                recipients: doc.to
+            }
+        };
+
+        HTTP.call(options.method, options.url, {
+            headers: options.headers,
+            params: options.form
+        }, (error, result) => {
+            if (error) { console.error(error); }
+            else {
+                if (result.data.resp_code === 'successful') {
+                    SmsRequests.update({
+                        _id: doc._id
+                    }, {
+                        $set: {
+                            status: 'scheduled'
+                        }
+                    });                
+                }
+            }
+        });
+    }
 });
 
 export default SmsRequests;
