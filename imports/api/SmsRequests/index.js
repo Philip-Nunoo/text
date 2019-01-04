2f1cd21a009d1abb892c5380e9ceeb0b19ea9f12
Philip Nunoo <phanunoo@gmail.com>
2019-01-04 11:41:09 +0000

    Added token to sms calls

--- imports/api/SmsRequests/index.js
+++ imports/api/SmsRequests/index.js
@@ -1,5 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 import { HTTP } from 'meteor/http';
+import Settings from './../Settings';
 
 const SmsRequests = new Mongo.Collection('sms-requests');
 
@@ -17,38 +18,44 @@ SmsRequests.after.insert(function (userId, doc) {
     console.log('send sms message to', doc.to);
     
     if (doc.to) {
-        var options = {
-            method: 'POST',
-            url: 'https://www.txtconnect.co/v2/app/api/send/sms.json',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            form: {
-                token: 'ffd452df80dd8b78154bc886b9ee03593202c1fc',
-                message: doc.message,
-                sender: 'TSF - Test',
-                recipients: doc.to
-            }
-        };
+        const setting = Settings.findOne({ active: true });
 
-        HTTP.call(options.method, options.url, {
-            headers: options.headers,
-            params: options.form
-        }, (error, result) => {
-            if (error) { console.error(error); }
-            else {
-                if (result.data.resp_code === 'successful') {
-                    SmsRequests.update({
-                        _id: doc._id
-                    }, {
-                        $set: {
-                            status: 'scheduled',
-                            transactionId: result.data.transaction_id
-                        }
-                    });                
+        if (setting && setting.sender && setting.txtGhanaSmsToken) {
+            var options = {
+                method: 'POST',
+                url: 'https://www.txtconnect.co/v2/app/api/send/sms.json',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                form: {
+                    token: setting.txtGhanaSmsToken,//'efd452df80dd8b78154bc886b9ee03593202c1fc',
+                    message: doc.message,
+                    sender: setting.sender, //'TSF - Test',
+                    recipients: doc.to
                 }
-            }
-        });
+            };
+
+            HTTP.call(options.method, options.url, {
+                headers: options.headers,
+                params: options.form
+            }, (error, result) => {
+                if (error) { console.error(error); }
+                else {
+                    if (result.data.resp_code === 'successful') {
+                        SmsRequests.update({
+                            _id: doc._id
+                        }, {
+                            $set: {
+                                status: 'scheduled',
+                                transactionId: result.data.transaction_id
+                            }
+                        });                
+                    }
+                }
+            });
+        } else {
+            console.log('no token');            
+        }
     }
 });
 
